473770a7e43ef6723a3aeae4a081d34420f1d439
omkardomb <104492258+odomb@users.noreply.github.com>
2024-01-10 20:54:18 +0530

    made some change

--- authapp/urls.py
+++ authapp/urls.py
@@ -3,5 +3,8 @@ from authapp import views
 
 urlpatterns = [
     path('', views.home,name="home"),
+    path('signup',views.signup,name="signup"),
+    path('login',views.handlelogin,name="handlelogin"),
+    
 ]
     
--- authapp/views.py
+++ authapp/views.py
@@ -4,3 +4,10 @@ from django.shortcuts import render
 def home(request):
     return render(request,"index.html")
 
+def signup(request):
+    return render(request,"signup.html")
+
+def handlelogin(request):
+    return render(request,"handlelogin.html")
+
+
