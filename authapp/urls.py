e435a3308f3feaab1bb55414660f7c518271ed40
omkardomb <104492258+odomb@users.noreply.github.com>
2024-01-11 11:35:19 +0530

    authentication done

--- authapp/urls.py
+++ authapp/urls.py
@@ -5,6 +5,7 @@ urlpatterns = [
     path('', views.home,name="home"),
     path('signup',views.signup,name="signup"),
     path('login',views.handlelogin,name="handlelogin"),
+    path('logout',views.handleLogout,name="handleLogout"),
     
 ]
     
